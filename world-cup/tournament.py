3dbfb059c54384fdfff55da511af439b4ea6e878
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-18 09:48:33 +0000

    Tue, Jan 18, 2022, 9:48 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -29,6 +29,7 @@ def main():
     # TODO: Simulate N tournaments and keep track of win counts
     
 
+
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
