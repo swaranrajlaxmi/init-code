ee75415bc6b92f7368b516235cd5ac8dcd9e8e61
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-19 07:04:48 +0000

    Wed, Jan 19, 2022, 7:04 AM +00:00

--- pset6/sentimental-hello/hello.py
+++ pset6/sentimental-hello/hello.py
@@ -1,5 +1,5 @@
 # a program that prints out a greeting to user.
 import importlib
 
-MyName = ("What is your name ?:")
-print("Hello," + MyName)
+MyName = input("What is your name ?:")
+print("Hello, " + MyName)
