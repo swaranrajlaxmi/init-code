674eb406b0a63f58066e9cb149d31bc51cc79c84
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-20 08:59:53 +0000

    Thu, Jan 20, 2022, 8:59 AM +00:00

--- pset6/cash.py
+++ pset6/cash.py
@@ -10,9 +10,22 @@ while True:
             break
     except ValueError:
         print("Enter a valid value")
-    # converting the user’s inputted dollars to cents
-    cents = round(int(dollar * 100))
-    coins = calculate_cents()
+# converting the user’s inputted dollars to cents
+cents = round(int(dollar * 100))
+count = 0
+while cents > 0:
+    if cents >= 25:
+        coins += 1
+        cents -= 25
+    elif cents >= 10:
+        coins += 1
+        cents -= 10
+    elif cents >= 5:
+        coins += 1
+        cents -= 5
+    elif cents >= 1:
+        coins += 1
+        cents -= 1
     print(coins)
 
 
