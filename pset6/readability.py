fae93e828b2d4d83678b6523c6ede4fc959ac2dd
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-20 11:25:43 +0000

    Thu, Jan 20, 2022, 11:25 AM +00:00

--- pset6/readability.py
+++ pset6/readability.py
@@ -9,8 +9,9 @@ sentences = words = letters = 0
 # count for letters, words, and sentences in the text
 for i in range(TextLength):
     if (text[i].isalpha()):
-        
-
+        letters += 1
+    if ():
+ print(letters)
 
 
 
