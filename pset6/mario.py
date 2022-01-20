0e3b369217b02df3fd88e065a19355c8302a8713
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-20 04:02:47 +0000

    Thu, Jan 20, 2022, 4:02 AM +00:00

--- pset6/mario.py
+++ pset6/mario.py
@@ -8,10 +8,7 @@ def main():
     height = get_height()
     for i in range(height):
         for j in range(height):
-            if j<(height-(i+1)):
-                print(" ", end="")
-            else:
-                print("#")
+            print("#")
 
 
 def get_height():
