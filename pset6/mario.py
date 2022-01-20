3ab7f1b42829c5d9cfd788545ea3d6a877dac4b4
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-20 05:35:52 +0000

    Thu, Jan 20, 2022, 5:35 AM +00:00

--- pset6/mario.py
+++ pset6/mario.py
@@ -3,4 +3,16 @@ import importlib
 
 def main():
     height = get_height()
-    #
+    # printing hash as like both right and left alighned pyramid
+
+
+def get_height():
+    # prompt the user for height untill the given condition is met
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0 and n <= 8:
+                break
+            print("Invalid")
+        except ValueError:
+            print("That's not an integer")
