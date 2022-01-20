abc3ea9fb74bfd23f947d780076e53d67ed4c7c0
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-20 09:04:01 +0000

    Thu, Jan 20, 2022, 9:04 AM +00:00

--- pset6/cash.py
+++ pset6/cash.py
@@ -29,27 +29,4 @@ while cents > 0:
 print(coins)
 
 
-def user_input():
-    while True:
-        try:
-            n = get_float("change owed: ")
-            if n > 0:
-                break
-        except ValueError:
-            print("Enter a valid value")
-    count = 0
-    while cents > 0:
-        if cents >= 25:
-            count += 1
-            cents -= 25
-        elif cents >= 10:
-            count += 1
-            cents -= 10
-        elif cents >= 5:
-            count += 1
-            cents -= 5
-        elif cents >= 1:
-            count += 1
-            cents -= 1
-
 
