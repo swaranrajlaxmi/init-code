5b0a072278c39c1626ec4196252be93a23125c11
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-20 06:32:30 +0000

    Thu, Jan 20, 2022, 6:32 AM +00:00

--- pset6/mario.py
+++ pset6/mario.py
@@ -5,12 +5,8 @@ def main():
     height = get_height()
     # printing hash as like both right and left alighned pyramid
     for i in range(height):
-        width = ((2 * height) + 2)
-        for j in range(width):
-            if j < (width/2) - (i + 1):
-                print(" ", end="")
-            else:
-                print("#", end="")
+        print((height - 1 - i) * " ", end="")
+        print((i + 1) * "#")
         print()
 
 
