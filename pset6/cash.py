93cedd6cd7e50b6977c59d32666672fe3af20edc
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-20 08:24:22 +0000

    Thu, Jan 20, 2022, 8:24 AM +00:00

--- pset6/cash.py
+++ pset6/cash.py
@@ -11,8 +11,19 @@ def main():
 
 def calculate_coins():
     count = 0
-    
-
+    while cents > 0:
+        while cents >= 25:
+            count += 1
+            cents -= 25
+        while cents >= 10:
+            count += 1
+            cents -= 10
+        while cents >= 5:
+            count += 1
+            cents -= 5
+        while cents >= 1:
+            count += 1
+            cents -= 1
 
 
 def get_dollar():
