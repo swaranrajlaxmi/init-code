ec148206cd7143d4cebfd7174d7a44028c642443
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-20 05:03:06 +0000

    Thu, Jan 20, 2022, 5:03 AM +00:00

--- pset6/mario.py
+++ pset6/mario.py
@@ -2,26 +2,27 @@
 
 import importlib
 
+
 def main():
     height = get_height()
     for i in range(height):
         for j in range(height):
-            if j<(height-(i+1)):
+            if j < (height-(i+1)):
                 print(" ", end="")
             else:
                 print("#", end="")
         print()
 
 
-
 def get_height():
     while True:
         try:
             n = int(input("Height: "))
-            if n>0 and n<=8:
+            if n > 0 and n <= 8:
                 break
         except ValueError:
             print("That's not an integer!")
     return n
+
 
 main()
