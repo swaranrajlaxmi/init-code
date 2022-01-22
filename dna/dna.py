8d450ced9274b9d47ed03fb78debcfa4fa9c34d0
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-22 10:37:37 +0000

    Sat, Jan 22, 2022, 10:37 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -11,7 +11,8 @@ def main():
     # TODO: Read database file into a variable
     with open(argv[1], "r") as csvfiles:
         reader = csv.DictReader(csvfile) # database is in a table format
-        ListOfDict = list(reader) #
+        ListOfDict = list(reader) # reader object stored as a list of dictionaries
+        
 
 
 
