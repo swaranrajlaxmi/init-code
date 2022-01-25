89fbbcc9ade42ef70e464810922b55eed821e41d
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-25 07:07:53 +0000

    Tue, Jan 25, 2022, 7:07 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -37,10 +37,10 @@ def main():
     # TODO: Check database for matching profiles
     with open(sys.argv[1], "r") as csvfile1:
         second_reader = csv.reader(csvfile1)
-        for column in second_reader:
-            print(column[1:])
-            if (max_count == column[1:]):
-                (print(f"{column[0]} is the match"))
+        for line in second_reader:
+            print(line[1:])
+            if (max_count == line[1:]):
+                (print(f"{line[0]} is the match"))
             else:
                 print("No match")
 
