1c6f45326ca8b917fa729df148b7c01d73414656
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-01-25 05:20:29 +0000

    Tue, Jan 25, 2022, 5:20 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -36,7 +36,7 @@ def main():
     with open(sys.argv[1], "r") as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
-            if (max_count == line[0:]):
+            if (max_count == line[1:]):
                 (print(f"{line[0]} is the match"))
         print("No match")
 
