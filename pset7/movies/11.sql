7da6697eabd13e71ac1d05864216dd221e411a91
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-03 08:24:10 +0000

    Thu, Feb 3, 2022, 8:24 AM +00:00

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN ratings ON ratings.movie
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON people.id = stars.person_id 
