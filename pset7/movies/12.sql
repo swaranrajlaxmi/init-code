507adf75037cb3234dc5fa73b83ac301141b9291
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-12 06:25:25 +0000

    Sat, Feb 12, 2022, 6:25 AM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,2 +1,5 @@
-SELECT title FROM movies JOIN stars ON stars.movie_id = movies.id JOIN people ON people.id = stars.person_id
+SELECT title FROM movies
+JOIN stars ON stars.movie_id = movies.id JOIN people ON people.id = stars.person_id
 WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
+
+
