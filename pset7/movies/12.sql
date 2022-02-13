88ffe288bc28e2bed623a51dd1859091162a2087
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-13 07:52:36 +0000

    Sun, Feb 13, 2022, 7:52 AM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -6,9 +6,9 @@ WHERE people.name = "Johnny Depp" AND title IN
 
 --SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Johnny Depp" OR name = "Helena Bonham Carter")
 
-SELECT title FROM movies
-JOIN stars ON stars.movie_id = movies.id
-JOIN people ON people.id = stars.person_id
+SELECT title FROM movies, stars, people
+WHERE stars.movie_id = movies.id
+AND people.id = stars.person_id
 GROUP BY title
 HAVING people.name LIKE "Johnny Depp"
     AND people.name LIKE "Helena Bonham Carter";
