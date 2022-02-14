28c75334f85431523838cab95345e80c9818eb10
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-14 14:31:56 +0000

    Mon, Feb 14, 2022, 2:31 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -13,3 +13,5 @@ AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id I
 
 SELECT city FROM airports WHERE id IN (SELECT destination_airport_id FROM flights WHERE id IN (SELECT flight_id FROM passengers
 WHERE passport_number IN (SELECT passport_number FROM people WHERE name LIKE 'Bruce')));
+
+SELECT id FROM phone_calls WHERE (SELECT phone_number FROM people WHERE name like "Bruce")
