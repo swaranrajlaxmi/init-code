522d16e05f81bbd79f3e7a7d88794a7dba1f1d0b
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-14 14:18:41 +0000

    Mon, Feb 14, 2022, 2:18 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -8,3 +8,7 @@ AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT a
 WHERE transaction_type LIKE "withdraw" AND month = 7 AND day = 28 AND atm_location LIKE "Leggett Street"))
 AND phone_number IN (SELECT caller FROM phone_calls WHERE month = 7 AND day = 28 AND duration < 60);
 AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE month = 7 AND day = 29 ORDER BY hour, minute ASC LIMIT 1));
+
+
+
+SELECT city FROM airports WHERE id IN (SELECT destination_airport_id FROM flights WHERE )
