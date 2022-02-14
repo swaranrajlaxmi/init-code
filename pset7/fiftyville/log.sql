d77026c56811ad8796e985013b8ee1a173f84083
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-14 13:55:27 +0000

    Mon, Feb 14, 2022, 1:55 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -5,4 +5,5 @@ WHERE license_plate IN
 -- get the license_plate of car within 10 min duration
 (SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity LIKE "exit")
 AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions
-WHERE transaction_type LIKE "withdraw" AND month = 7 AND day = 28 AND atm_location LIKE "Leggett Street"));
+WHERE transaction_type LIKE "withdraw" AND month = 7 AND day = 28 AND atm_location LIKE "Leggett Street"))
+AND 
