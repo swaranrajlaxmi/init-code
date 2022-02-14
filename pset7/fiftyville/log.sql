07da9570966d4c47251dda5a3f743a530be2e805
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-14 13:58:20 +0000

    Mon, Feb 14, 2022, 1:58 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -6,4 +6,4 @@ WHERE license_plate IN
 (SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity LIKE "exit")
 AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions
 WHERE transaction_type LIKE "withdraw" AND month = 7 AND day = 28 AND atm_location LIKE "Leggett Street"))
-AND 
+AND phone_number IN (SELECT caller FROM phone_calls WHERE month = 7 AND day = 28 AND duration < 60);
