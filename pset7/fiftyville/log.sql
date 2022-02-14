0e05c7bb2209765fd4ed27eedaf0d6e91655cc67
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-14 13:40:38 +0000

    Mon, Feb 14, 2022, 1:40 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -1,3 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+-- getting name of thief
 SELECT name FROM people WHERE license_plate IN
-(SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity LIKE "exit";)
+-- get the license_plate of car 
+(SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute > 15 AND minute < 25 AND activity LIKE "exit");
