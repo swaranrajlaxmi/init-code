d693d7f8128bc6107875e7c07769d8bba620b389
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-02-15 03:08:10 +0000

    Tue, Feb 15, 2022, 3:08 AM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -7,12 +7,12 @@ WHERE license_plate IN
 -- get the id of thief by linking atm_transaction and bank_account_number
 AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions
 WHERE transaction_type LIKE "withdraw" AND month = 7 AND day = 28 AND atm_location LIKE "Leggett Street"))
--- grt the phone_number of thief 
+-- get the phone_number of thief
 AND phone_number IN (SELECT caller FROM phone_calls WHERE month = 7 AND day = 28 AND duration < 60);
 AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE month = 7 AND day = 29 ORDER BY hour, minute ASC LIMIT 1));
 
 
-
+-- 
 SELECT city FROM airports WHERE id IN (SELECT destination_airport_id FROM flights WHERE id IN (SELECT flight_id FROM passengers
 WHERE passport_number IN (SELECT passport_number FROM people WHERE name LIKE 'Bruce')));
 
