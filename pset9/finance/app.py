98b93edeb442a42e430b153ba7ad636882fb7134
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-04-11 11:17:11 +0000

    Mon, Apr 11, 2022, 11:17 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -117,7 +117,10 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    if (request.method == "POST"):
+        pass
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
