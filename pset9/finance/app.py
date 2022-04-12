67f53f5b2a92bdd75db856bb4eaf37a0e6f77dd1
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-04-12 08:26:53 +0000

    Tue, Apr 12, 2022, 8:26 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -121,7 +121,7 @@ def quote():
 
         if not stock:
             return apology("Please enter a valid symbol")
-        return render_template("quoted.html")
+        return render_template("quoted.html", stock=stock)
     else:
         return render_template("quote.html")
 
