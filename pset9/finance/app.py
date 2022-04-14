e76f5d334bc733592b8270ee9b34db198c87401e
swaranrajlaxmi <87296076+swaranrajlaxmi@users.noreply.github.com>
2022-04-14 07:45:26 +0000

    Thu, Apr 14, 2022, 7:45 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -84,7 +84,7 @@ def buy():
         else:
             db.execute("UPDATE users SET cash = ? WHERE id = ?", cash - total, user_id)
             db.execute("INSERT INTO transactions (user_id, stock_name, price, shares, bought_or_sold, symbol) VALUES(?,?,?,?,?,?)",
-                        user_id, name, price, shares, buy, symbol)
+                        user_id, name, price, shares, 'buy', symbol)
         return redirect("/")
 
     else:
